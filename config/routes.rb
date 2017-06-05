827bbb005479fb48323ca568d6ed8b1bfd6e000e
James <james@jkamradcliffe.net>
2017-06-05 18:36:06 +0100

    Add home, about, contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-	root 'application#hello'
+	root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
