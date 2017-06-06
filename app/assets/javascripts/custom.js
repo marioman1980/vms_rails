dac4ca49c34f3ce1aae1ac6ee496b6dc5c21c012
James <james@jkamradcliffe.net>
2017-06-06 10:37:05 +0100

    Allow nav to be positioned correctly on load

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,13 +3,23 @@ window.onload = function () {
 }
 
 // Position nav at bottom of header when not collapsed
+var positionNav = function() {
+	if ($( window ).width() >= 768) {
+		$('#nav-links').addClass('header-align-bottom');
+	}
+	else {
+		$('#nav-links').removeClass('header-align-bottom');
+	}		
+}
+
+
+
+
 $(document).ready(function(){
 	$(window).resize(function() {
-		if ($( window ).width() >= 768) {
-			$('#nav-links').addClass('header-align-bottom');
-		}
-		else {
-			$('#nav-links').removeClass('header-align-bottom');
-		}		
+		positionNav();	
 	});
+	$(window).load(function() {
+		positionNav();	
+	});	
 });
