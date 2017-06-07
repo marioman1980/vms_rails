934a24c63aaa318b9b6125f5c17357c5468c9909
James <james@jkamradcliffe.net>
2017-06-07 14:28:10 +0100

    Create User controller. Add named route and verify home page link

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
 	root 'pages#home'
-
-  get '/help', 					to: 'pages#help'
-
+  get '/help', 						to: 'pages#help'
   get '/contact', 				to: 'pages#contact'
-
 	get '/privacy_policy', 	to: 'pages#privacy_policy'
+	get '/register',				to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
